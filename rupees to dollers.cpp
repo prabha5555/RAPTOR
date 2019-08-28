23ec79d401ff81905bc1a08f8c922faef5fda794
prabha5555 <mallikapeddamallu@gmail.com>
2019-08-28 15:26:18 +0530

    raptor files

---
+++ rupees to dollers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? res;
+   ?? r;
+
+   raptor_prompt_variable_zzyz ="enter amount in rupees";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> r;
+   res =r/73;
+   cout << res << endl;
+   return 0;
+}
